7008d3c258185dd3810832dbe0f2d701839a683f
Gcat101 <gcatmail2@gmail.com>
2023-03-27 20:59:42 +0300

    update the install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ if [ "$EUID" -ne 0 ] then
     sudo su
 fi
 
+rm /usr/local/bin/kjspkg
+
 curl -s https://raw.githubusercontent.com/Modern-Modpacks/kjspkg/main/app.py > /usr/local/bin/kjspkg
 pip -q install $(curl -s https://raw.githubusercontent.com/Modern-Modpacks/kjspkg/main/requirements.txt)
 chmod +x /usr/local/bin/kjspkg
