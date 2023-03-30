fc26866e465cdf28635ec6bb48a9980e6fdccfc7
Gcat101 <gcatmail2@gmail.com>
2023-03-30 19:56:03 +0300

    Boss tools oxygen system

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def pkginfo(pkg:str): # Print info about a pkg
 {info["description"]}
 
 {_bold("Dependencies")}: {", ".join([i.title() for i in info["dependencies"]]) if "dependencies" in info.keys() and len(info["dependencies"])>0 else "*nothing here*"}
-{_bold("Incompatibilities")}: {", ".join([i.title() for i in info["incompatibilities"]]) if "incompatibilities" in info.keys() and len(info["incompatibilities"])>0 else "*nothing here*"}
+{_bold("Incompatibilities")}: {", ".join([i.title() for i in info["incompatibilities"]]) if "incompatibilities" in info.keys() and len(info["incompatibilities"])>0 else "*compatible with everything!*"}
 
 {_bold("License")}: {info["license"]}
 {_bold("GitHub")}: https://github.com/{info["repo"]}/tree/{info["branch"]}
