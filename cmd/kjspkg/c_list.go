fa4454d7593d3f252c3102d13b8a256de4157f3d
tizu69 <60812901+tizu69@users.noreply.github.com>
2024-11-14 21:46:54 +0100

    refactor:cli/move-related-into-omnifile

--- cmd/kjspkg/c_list.go
+++ cmd/kjspkg/c_list.go
@@ -46,3 +46,26 @@ func (c *CList) Run(ctx *Context) error {
 	}
 	return nil
 }
+
+type CListall struct {
+	Search string `help:"The search query"`
+}
+
+func (c *CListall) Run(ctx *Context) error {
+	cmd := CList{
+		All:    true,
+		Search: c.Search,
+	}
+	return cmd.Run(ctx)
+}
+
+type CSearch struct {
+	Query string `arg:"" help:"The search query"`
+}
+
+func (c *CSearch) Run(ctx *Context) error {
+	cmd := CListall{
+		Search: c.Query,
+	}
+	return cmd.Run(ctx)
+}
