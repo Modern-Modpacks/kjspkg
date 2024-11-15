70e885585f5667734c3de864e550b7d3255d193f
tizu69 <60812901+tizu69@users.noreply.github.com>
2024-11-15 21:04:40 +0100

    fix:cli/selector-ui-value-transfer

--- cmd/kjspkg/selectors.go
+++ cmd/kjspkg/selectors.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"slices"
+
 	"github.com/charmbracelet/huh"
 )
 
@@ -23,6 +25,37 @@ func NewSelect[T comparable](
 	err := huh.NewSelect[T]().
 		Title(title).
 		Options(options...).
+		Value(selected).
+		WithTheme(huh.ThemeBase16()).
+		Run()
+	return err
+}
+
+// NewMultiSelect is a wrapper function that displays a selection prompt only if
+// the provided variable doesn't already match one of the available options.
+func NewMultiSelect[T comparable](
+	title string,
+	getOptions func(opts *[]huh.Option[T]),
+	selected *[]T,
+) error {
+	options := []huh.Option[T]{}
+	getOptions(&options)
+
+	allValid := true
+	for _, opt := range *selected {
+		if !slices.ContainsFunc(options, func(e huh.Option[T]) bool { return e.Value == opt }) {
+			allValid = false
+			break
+		}
+	}
+	if allValid {
+		return nil
+	}
+
+	err := huh.NewMultiSelect[T]().
+		Title(title).
+		Options(options...).
+		Value(selected).
 		WithTheme(huh.ThemeBase16()).
 		Run()
 	return err
